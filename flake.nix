22e04812bfbb321f9d559b2d1f4af6ffd1dd4b14
Joe Neeman <joeneeman@gmail.com>
2024-03-03 08:32:43 -0600

    Update the flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,35 @@
 {
-  description = "A devShell example";
+  description = "Basic rust dev shell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
-    unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, unstable, ... }:
+  outputs = { nixpkgs, fenix, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ fenix.overlays.default ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        unstablePkgs = import unstable {
-          localSystem = { inherit system; };
-        };
+        rust-toolchain = pkgs.fenix.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ];
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
-            unstablePkgs.rust-analyzer
-            rust-bin.stable.latest.default
+            cargo-outdated
+            cargo-fuzz
+            rust-toolchain
           ];
-          
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-          '';
         };
       }
     );
